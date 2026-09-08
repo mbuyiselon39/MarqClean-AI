74f67144f8e37e19c5f5355d36aeb97915f53a38
Mbuyiselo Ndlela <mbuyiselon39@gmail.com>
2026-09-08 14:57:40 +0200

    feat: apply MarqClean AI brand system and SEO at app bootstrap

--- main.tsx
+++ main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MotionConfig } from "framer-motion";
 import "./index.css";
+import "./brand.css";
 import App from "./App";
+import { installBrandSeo } from "./seo";
+
+installBrandSeo();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
